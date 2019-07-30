16d9ac4900f8ebed8ef5119adfcf06335604a423
AshleenRana <ashleen261@gmail.com>
2019-07-30 13:44:53 -0400

    sp

--- assignment5/SplashScreen.Designer.cs
+++ assignment5/SplashScreen.Designer.cs
@@ -29,40 +29,57 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
             this.SplashTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Loadinglabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 57F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 155);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(729, 99);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to my app";
             // 
             // SplashTimer
             // 
             this.SplashTimer.Interval = 3000;
             this.SplashTimer.Tick += new System.EventHandler(this.SplashTimer_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::assignment5.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(87, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(591, 281);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Loadinglabel
+            // 
+            this.Loadinglabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Loadinglabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Loadinglabel.Location = new System.Drawing.Point(305, 395);
+            this.Loadinglabel.Name = "Loadinglabel";
+            this.Loadinglabel.Size = new System.Drawing.Size(190, 50);
+            this.Loadinglabel.TabIndex = 1;
+            this.Loadinglabel.Text = "Loading...";
+            // 
             // SplashScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.Loadinglabel);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
             this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashScreen";
             this.Load += new System.EventHandler(this.SplashScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer SplashTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Loadinglabel;
     }
 }
